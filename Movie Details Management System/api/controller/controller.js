75ffd20cb965a9b2260bb8e338c7fe887942c1c0
dexter-idc <140709325+Parthipan-olt@users.noreply.github.com>
2023-11-03 10:05:59 +0530

    Updated routes

--- Movie Details Management System/api/controller/controller.js
+++ Movie Details Management System/api/controller/controller.js
@@ -2,6 +2,21 @@ const renderIndexPage = async (req, res) => {
   res.render('index');
 };
 
+const renderEditPage = async (req, res) => {
+  res.render('edit');
+};
+
+const renderListPage = async (req, res) => {
+  res.render('list');
+};
+
+const renderDetailsPage = async (req, res) => {
+  res.render('details');
+};
+
 module.exports = {
   renderIndexPage,
+  renderListPage,
+  renderEditPage,
+  renderDetailsPage,
 };
--- Movie Details Management System/routes/route.js
+++ Movie Details Management System/routes/route.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const controller = require('../api/controller/controller');
 
 router.get('/', controller.renderIndexPage);
+router.use('/submit', controller.renderListPage);
+router.get('/view', controller.renderDetailsPage);
+router.get('/edit', controller.renderEditPage);
+
 
 module.exports = router;
